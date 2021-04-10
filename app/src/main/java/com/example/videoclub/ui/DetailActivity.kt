8db0c4778b5ded068484e0f22885f8c46dae76b1
AlmudenaRA <almudena.ruedas@gmail.com>
2021-04-10 15:59:10 +0200

    Diseño detalle de la pelicula

---
+++ app/src/main/java/com/example/videoclub/ui/DetailActivity.kt
@@ -0,0 +1,18 @@
+package com.example.videoclub.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.videoclub.R
+
+
+class DetailActivity: AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+
+    }
+
+
+}
